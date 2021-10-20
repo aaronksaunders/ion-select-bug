532049f0a46185b1560899ef936e3006338b46f8
Aaron Saunders <aaron@clearlyinnovative.com>
2021-10-20 16:31:57 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'modal-bug',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
